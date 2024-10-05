f4cd258ae43fff136ef2fce346704ae0080e28cb
FlamedDogo99 <96555444+FlamedDogo99@users.noreply.github.com>
2024-10-05 10:36:16 -0600

    Fixed spacing issue

--- changes/commands/math/advancedSymbols.ts
+++ changes/commands/math/advancedSymbols.ts
@@ -1,8 +1,8 @@
 LatexCmds['≢'] = 
   LatexCmds.nequiv =
   LatexCmds.notequiv =
-  bindBinaryOperator('\\nequiv', '&#8802;', 'not equivalent to');
+  bindBinaryOperator('\\nequiv ', '&#8802;', 'not equivalent to');
 LatexCmds['∤'] = 
   LatexCmds.nmid =
   LatexCmds.notmid =
-  bindBinaryOperator('\\nmid', '&#8740;', 'does not divide');
+  bindBinaryOperator('\\nmid ', '&#8740;', 'does not divide');
